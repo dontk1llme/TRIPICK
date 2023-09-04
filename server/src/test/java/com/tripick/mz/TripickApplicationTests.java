a88bf203b3c187b2a837773c2930082b2c0d126f
tjdwnsmm <tjdwnsmm@naver.com>
2023-09-04 14:39:20 +0900

    init: spring architecture designed

---
+++ server/src/test/java/com/tripick/mz/TripickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripick.mz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
