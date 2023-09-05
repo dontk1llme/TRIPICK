d08251e1da4943d89eea3381509ae98839fc05e8
Hyunsoo <imapples@naver.com>
2023-09-05 10:36:45 +0900

    feat: Add skeleton code

---
+++ front_temp/frontend/src/style/Globalstyles.js
@@ -0,0 +1,93 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+    //#region 공용 css 설정
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        -webkit-text-size-adjust: none; 
+        -moz-text-size-adjust: none; 
+        -ms-text-size-adjust: none; 
+      
+    }
+    html,body {
+        font-weight: 400;
+        font-size: 12px;
+        width: 100%;
+        height: 100%;
+
+    }
+    a {
+        cursor: pointer;
+        text-decoration: none;
+        color: black;
+    }
+    img {
+        border: 0;
+        vertical-align: middle;
+        image-rendering: -webkit-optimize-contrast !important;
+        backface-visibility:hidden !important;
+    }
+    ul,li {
+        list-style: none;
+    }
+    button {
+        border: none;
+        cursor: pointer;
+    }
+
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    font-family: 'Pretendard', 'Noto Sans KR', sans-serif;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+    display: block;
+    }
+    body {
+    line-height: 1;
+    }
+    ol, ul {
+    list-style: none;
+    }
+    blockquote, q {
+    quotes: none;
+    }
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+    content: '';
+    content: none;
+    }
+    table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    }
+    button {
+        background-color: #fff;
+    }
+    //#endregion
+    
+`;
+
+export default GlobalStyles;
---
+++ front_temp/frontend/src/utils/utilsConstant.js
@@ -0,0 +1,10 @@
+export const URL = {
+    LOGIN: {
+        LOGIN: '/login',
+    },
+    HOME: {
+        MAIN: '/',
+    },
+};
+
+export const MAX_WIDTH = '1920px';
