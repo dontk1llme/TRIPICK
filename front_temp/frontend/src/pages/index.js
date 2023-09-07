d3c24c08df1e49a2ca80b5097e97ecaf3f5bc81b
dontk1llme <letm2go@gmail.com>
2023-09-07 17:09:04 +0900

    merge: index.js

--- front_temp/frontend/src/pages/index.js
+++ front_temp/frontend/src/pages/index.js
@@ -1,8 +1,5 @@
 export { default as Landing } from './landing/Landing';
 export { default as Cart } from './cart/Cart';
 export { default as Diary } from './diary/Diary';
-<<<<<<< HEAD
 export { default as MyPage } from './mypage/MyPage';
-=======
 export { default as Recommend } from './recommend/Recommend';
->>>>>>> f205fe464900a06edec47a63f913735683374f05
