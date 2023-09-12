c06b48fc83795b5fcacadcd8c1b723f5dd821212
Hyunsoo <imapples@naver.com>
2023-09-12 14:06:14 +0900

    feat: add toptab shortcuts

--- front_temp/frontend/src/components/common/TopTab.jsx
+++ front_temp/frontend/src/components/common/TopTab.jsx
@@ -12,6 +12,32 @@ const TopTab = () => {
             <S.Logo onClick={() => navigate(utils.URL.HOME.MAIN)}>
                 <img src={require('asset/images/logo.png').default} alt="logo" />
             </S.Logo>
+            <S.ShortCut>
+                <S.Liked onClick={() => navigate(utils.URL.CART.LIST)}>
+                    <svg width="37" height="34" viewBox="0 0 37 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M26.7174 1C23.1413 1 20.0522 3.09475 18.5 6.15025C16.9478 3.09475 13.8587 1 10.2826 1C5.15435 1 1 5.29975 1 10.5917C1 15.8837 4.18043 20.7347 8.28913 24.7195C12.3978 28.7042 18.5 32.5 18.5 32.5C18.5 32.5 24.4043 28.7672 28.7109 24.7195C33.3065 20.404 36 15.8995 36 10.5917C36 5.284 31.8457 1 26.7174 1Z"
+                            fill="#8390FA"
+                            fill-opacity="0.16"
+                        />
+                        <path
+                            d="M26.7174 1C23.1413 1 20.0522 3.09475 18.5 6.15025C16.9478 3.09475 13.8587 1 10.2826 1C5.15435 1 1 5.29975 1 10.5917C1 15.8837 4.18043 20.7347 8.28913 24.7195C12.3978 28.7042 18.5 32.5 18.5 32.5C18.5 32.5 24.4043 28.7672 28.7109 24.7195C33.3065 20.404 36 15.8995 36 10.5917C36 5.284 31.8457 1 26.7174 1Z"
+                            stroke="#8390FA"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                </S.Liked>
+                <S.Profile onClick={() => navigate(utils.URL.MYPAGE.PASSPORT)}>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16 16C20.42 16 24 12.42 24 8C24 3.58 20.42 0 16 0C11.58 0 8 3.58 8 8C8 12.42 11.58 16 16 16ZM16 20C10.66 20 0 22.68 0 28V32H32V28C32 22.68 21.34 20 16 20Z"
+                            fill="#8390FA"
+                        />
+                    </svg>
+                </S.Profile>
+            </S.ShortCut>
         </S.Wrap>
     );
 };
@@ -32,6 +58,43 @@ const S = {
         & > img {
             width: auto;
             max-height: 32px;
+        }
+    `,
+    ShortCut: styled.div`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 99px;
+        height: 100%;
+    `,
+    Liked: styled.div`
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+        & svg {
+            width: 100%;
+            height: 100%;
+        }
+
+        &:hover {
+            & svg {
+                color: ${({ theme }) => theme.color.main1};
+            }
+        }
+    `,
+    Profile: styled.div`
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+        & svg {
+            width: 100%;
+            height: 100%;
+            color: ${({ theme }) => theme.color.main1};
+        }
+        &:hover {
+            & svg {
+                color: ${({ theme }) => theme.color.main2};
+            }
         }
     `,
 };
