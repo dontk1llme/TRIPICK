218c4a809b0c24714efdacf8ea28429566d683f0
fctm212 <fctnm212@naver.com>
2023-09-13 10:22:33 +0900

    feat: calendar min max added, css edit

--- front_temp/frontend/src/components/recommend/Calendar.jsx
+++ front_temp/frontend/src/components/recommend/Calendar.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactCalendar from 'react-calendar';
 import styles from './calendar.css';
 import moment from "moment";
 import { IoCalendarClearSharp } from 'react-icons/io5';
 
 const CalendarComponent = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const today = new Date();
+  const sixMonthsLater = new Date();
+  sixMonthsLater.setMonth(today.getMonth() + 6);
+
+  const [selectedDate, setSelectedDate] = useState([today, today]); // 초기 선택 날짜 설정
 
   const formatDate = (date) => {
     return moment(date).format("YYYY년 MM월 DD일");
   };
 
-  // 첫 시작 시 이상한 border css를 고치려고 해 보았지만... 
+  // 첫 시작 시 이상한 border css를 고치려고 해 보았지만...
   const tileContent = ({ date, view }) => {
-    const isInRange = view === 'month' && date >= selectedDate[0] && date <= selectedDate[1];
+    const isInRange = view === 'month' && date >= today && date <= sixMonthsLater;
     const tileClasses = []; // 기본 클래스 추가
 
     if (isInRange) {
@@ -25,22 +29,17 @@ const CalendarComponent = () => {
     );
   };
 
+  useRef
 
   return (
     <div style={{ margin: '30px' }}>
       <div className='calendar'>
-        {selectedDate.length > 0 ? (
-          <p className='text-center'>
-            <IoCalendarClearSharp style={{ marginRight: '10px' }} />
-            {formatDate(selectedDate[0])}
-            &nbsp; - &nbsp;
-            {formatDate(selectedDate[1])}
-          </p>
-        ) : (
-          <p className='text-center'>
-            <span className='bold'> 여행 날짜를 선택하세요</span>
-          </p>
-        )}
+        <p className='text-center'>
+          <IoCalendarClearSharp style={{ marginRight: '10px' }} />
+          {formatDate(selectedDate[0])}
+          
+          {selectedDate[1]!=selectedDate[0] ? ' - ' + formatDate(selectedDate[1]) : ''}
+        </p>
         <br />
 
         <div className='styles.calendarContainer'>
@@ -50,6 +49,8 @@ const CalendarComponent = () => {
             selectRange={true}
             formatDay={(locale, date) => moment(date).format("DD")}
             tileContent={tileContent} // tileContent 함수를 전달
+            minDate={today} // 오늘 이후만 선택 가능하도록 설정
+            maxDate={sixMonthsLater} // 6개월 이내까지만 표시
           />
         </div>
       </div>
