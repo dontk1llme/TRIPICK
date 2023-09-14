4d207ce03a7a49a0b6daa5961d8a3a2304f32ace
tjdwnsmm <tjdwnsmm@naver.com>
2023-09-14 15:31:39 +0900

    chore: SecurityConfig modified

--- server/spring/src/main/java/com/tripick/mz/common/config/SecurityConfig.java
+++ server/spring/src/main/java/com/tripick/mz/common/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.tripick.mz.common.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,9 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CorsConfig corsConfig;
-
-    @Value("${jwt.secret}")
-    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
