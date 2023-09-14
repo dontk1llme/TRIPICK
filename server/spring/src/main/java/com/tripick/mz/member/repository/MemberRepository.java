5b249864ca23d362778bcea0b38fd0b86a090bd5
ooyouj <ooyouj@naver.com>
2023-09-14 15:52:21 +0900

    feat : Add getMember, Update nickName, Update profileImage

---
+++ server/spring/src/main/java/com/tripick/mz/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.tripick.mz.member.repository;
+
+import com.tripick.mz.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
