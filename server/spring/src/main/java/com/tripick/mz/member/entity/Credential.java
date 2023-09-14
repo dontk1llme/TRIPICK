2009cb1b38bbf603da471515a543ea973c3634fa
ooyouj <ooyouj@naver.com>
2023-09-14 15:52:21 +0900

    feat : Add getMember, Update nickName, Update profileImage

--- server/spring/src/main/java/com/tripick/mz/member/entity/Credential.java
+++ server/spring/src/main/java/com/tripick/mz/member/entity/Credential.java
@@ -2,12 +2,14 @@ package com.tripick.mz.member.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
---
+++ server/spring/src/main/java/com/tripick/mz/member/service/MemberService.java
@@ -0,0 +1,24 @@
+package com.tripick.mz.member.service;
+
+import com.tripick.mz.S3.dto.S3FileDto;
+import com.tripick.mz.member.dto.request.UpdateNicknameRequestDto;
+import com.tripick.mz.member.dto.response.MemberResponseDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface MemberService {
+    //내 정보 조회
+    MemberResponseDto getMemberById(int memberId);
+    //닉네임 변경
+    MemberResponseDto updateNickname(UpdateNicknameRequestDto updateNicknameRequestDto);
+
+    //프로필 사진 변경
+    List<S3FileDto> updateImage(List<MultipartFile> multipartFiles, int memberId);
+
+    //프로필 사진 삭제
+    String deleteImage(int memberId);
+
+    //대표 뱃지 변경
+    void updateMainBadge(int memberId, int badgeId);
+}
