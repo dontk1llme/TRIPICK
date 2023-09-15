0b5459464d521af6d79df91bb86f5ea49c3f660a
dontk1llme <letm2go@gmail.com>
2023-09-15 16:23:11 +0900

    feat: google login

--- front_temp/frontend/src/App.js
+++ front_temp/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { CountryProvider } from './components/diary/CountryContext';
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 import * as styles from 'style';
 import * as utils from 'utils';
@@ -12,20 +13,22 @@ const App = () => {
     return (
         <ThemeProvider theme={styles.Theme}>
             <CountryProvider>
+            <GoogleOAuthProvider  clientId={`${process.env.REACT_APP_GOOGLE_REST_API_KEY}`}>
                 <styles.GlobalStyles />
-                <Router>
-                    <Routes>
-                        <Route path={utils.URL.HOME.MAIN} element={<components.Layout />}>
-                            <Route path={utils.URL.HOME.LANDING} element={<pages.Landing />}></Route>
-                            <Route path={utils.URL.CART.LIST} element={<pages.Cart />}></Route>
-                            <Route path={utils.URL.MYPAGE.PASSPORT} element={<pages.MyPage />}></Route>
-                            <Route path={utils.URL.MYPAGE.DIARY} element={<pages.Diary />}></Route>
-                            <Route path={utils.URL.RECOMMEND.CALENDAR} element={<pages.Recommend />}></Route>
-                            <Route path={utils.URL.CART.COMPARE} element={<pages.Compare />}></Route>
-                            <Route path={utils.URL.DETAIL.ABOUT} element={<pages.Detail />}></Route>
-                        </Route>
-                    </Routes>
-                </Router>
+                    <Router>
+                        <Routes>
+                            <Route path={utils.URL.HOME.MAIN} element={<components.Layout />}>
+                                <Route path={utils.URL.HOME.LANDING} element={<pages.Landing />}></Route>
+                                <Route path={utils.URL.CART.LIST} element={<pages.Cart />}></Route>
+                                <Route path={utils.URL.MYPAGE.PASSPORT} element={<pages.MyPage />}></Route>
+                                <Route path={utils.URL.MYPAGE.DIARY} element={<pages.Diary />}></Route>
+                                <Route path={utils.URL.RECOMMEND.CALENDAR} element={<pages.Recommend />}></Route>
+                                <Route path={utils.URL.CART.COMPARE} element={<pages.Compare />}></Route>
+                                <Route path={utils.URL.DETAIL.ABOUT} element={<pages.Detail />}></Route>
+                            </Route>
+                        </Routes>
+                    </Router>
+                </GoogleOAuthProvider>
             </CountryProvider>
         </ThemeProvider>
     );
