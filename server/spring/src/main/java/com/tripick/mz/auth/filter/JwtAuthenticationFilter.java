f092de9c28e6e5a8f431841a4391fa443e829dcc
허성백 <heosb123@hanmail.net>
2023-09-15 18:17:26 +0900

    feat: social login config, token provider, utils added

--- server/spring/src/main/java/com/tripick/mz/auth/filter/JwtAuthenticationFilter.java
+++ server/spring/src/main/java/com/tripick/mz/auth/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,20 @@
 package com.tripick.mz.auth.filter;
 
-import com.tripick.mz.auth.util.AuthTokenProvider;
+import com.tripick.mz.auth.token.AuthToken;
+import com.tripick.mz.auth.token.AuthTokenProvider;
+import com.tripick.mz.auth.util.JwtHeaderUtil;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Log4j2
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -22,7 +28,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     final String authorizationHeader = request.getHeader("Authorization");
 
     if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
-      String tokenStr = JwtHeaderUtil.getAccessToken
+      String tokenStr = JwtHeaderUtil.getAccessToken(request);
+      AuthToken token = tokenProvider.convertAuthToken(tokenStr);
+
+      if(token.validate()){
+        Authentication authentication = tokenProvider.getAuthentication(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+
+      filterChain.doFilter(request, response);
     }
 
   }
--- server/spring/src/main/java/com/tripick/mz/auth/util/AuthTokenProvider.java
+++
@@ -1,5 +0,0 @@
-package com.tripick.mz.auth.util;
-
-public class AuthTokenProvider {
-
-}
