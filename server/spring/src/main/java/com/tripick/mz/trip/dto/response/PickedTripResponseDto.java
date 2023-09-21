6861ec7386edde3c83d9abc85fe4c07198463bf8
tjdwnsmm <tjdwnsmm@naver.com>
2023-09-21 17:41:41 +0900

    feat: add trip pick, unpick, retrieve API

---
+++ server/spring/src/main/java/com/tripick/mz/trip/dto/response/PickedTripResponseDto.java
@@ -0,0 +1,24 @@
+package com.tripick.mz.trip.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class PickedTripResponseDto {
+    private String id;
+    private String city;
+    private String country;
+    private String startDate;
+    private String endDate;
+    private int traveler;
+    private float exchangeRate;
+    private float priceIndex;
+    private float crimeRate;
+    private int flightFee;
+    private boolean activated;
+}
