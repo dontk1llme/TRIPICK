8a0fe384dfae7704043d4a74b4c476873e3bc909
fctm212 <fctnm212@naver.com>
2023-09-25 11:38:18 +0900

    style: opening temp

--- front_temp/frontend/src/pages/opening/Opening.jsx
+++ front_temp/frontend/src/pages/opening/Opening.jsx
@@ -1,35 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import * as components from 'components';
-import * as hooks from 'hooks';
-import img1 from '../../asset/images/opening1.gif'
+import img1 from '../../asset/images/opening1.gif';
 
 const Opening = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    return ( 
-    <S.Wrap>
-      <S.Image src={img1}></S.Image>
-    </S.Wrap>);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <S.Wrap onClick={handleClick}>
+      <S.BoxLeft isOpen={isOpen}>
+        {/* 좌측 박스 내용 */}
+      </S.BoxLeft>
+      <S.Image src={img1} />
+      <S.BoxRight isOpen={isOpen}>
+        {/* 우측 박스 내용 */}
+      </S.BoxRight>
+    </S.Wrap>
+  );
 };
 
-
-
 const S = {
-    Wrap: styled.div`
-        display: flex;
-        width: 100%;
-        height: 100%;
-        margin: 60px 0px;
-    `,
-    Image: styled.img`
-        // width: 900px;
-        // height: 690px;
-        width: 100%; /* 부모 요소인 LeftContainer에 꽉 차도록 설정 */
-        height: 100%;
-        position: absolute; /* absolute 포지션 설정 */
-        top: 0;
-        left: 0; //toptab으로 못 넘어감. . .
-    `,
+  Wrap: styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+    transition: all 1s ease-in-out; /* 애니메이션 효과를 부드럽게 적용 */
+  `,
+  Image: styled.img`
+    width: 100%;
+    height: 100%;
+  `,
+  BoxLeft: styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: ${(props) => (props.isOpen ? '0' : '50%')}; /* isOpen 상태에 따라 너비 변경 */
+    height: 100%;
+    background-color: #212121;
+    opacity: ${(props) => (props.isOpen ? '0' : '0.8')}; /* isOpen 상태에 따라 투명도 변경 */
+    z-index: 99999;
+    transition: all 1s ease-in-out; /* 애니메이션 효과를 부드럽게 적용 */
+  `,
+  BoxRight: styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: ${(props) => (props.isOpen ? '0' : '50%')}; /* isOpen 상태에 따라 너비 변경 */
+    height: 100%;
+    background-color: #212121;
+    opacity: ${(props) => (props.isOpen ? '0' : '0.8')}; /* isOpen 상태에 따라 투명도 변경 */
+    z-index: 99999;
+    transition: all 1s ease-in-out; /* 애니메이션 효과를 부드럽게 적용 */
+  `,
 };
 
 export default Opening;
