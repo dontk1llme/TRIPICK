489b68b21bd1d3d7368851095c0a97727107f6d5
허성백 <heosb123@hanmail.net>
2023-09-29 21:38:39 +0900

    fix: 카카오 로그인 redirect-uri 수정
    Former-commit-id: 83a3e782ccdd339853eed5aff278fba3009d73e8

--- front_temp/frontend/src/components/common/LoginModal.jsx
+++ front_temp/frontend/src/components/common/LoginModal.jsx
@@ -15,7 +15,7 @@ const LoginModal = ({ setModalOpen }) => {
 
     ///////////// kakao ////////////////
     const Rest_api_key = process.env.REACT_APP_KAKAO_REST_API_KEY; //REST API KEY
-    const redirect_uri = 'http://localhost:3000/auth'; //Redirect URI
+    const redirect_uri = 'https://tripick.site/auth'; //Redirect URI
     // oauth 요청 URL
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
