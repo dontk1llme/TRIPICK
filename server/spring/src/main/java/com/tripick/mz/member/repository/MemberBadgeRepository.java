57f3e74a047f71c36f4518f9cfe01b4ffba6cbda
허성백 <heosb123@hanmail.net>
2023-10-01 20:35:25 +0900

    fix: 메인뱃지 1번으로 수정, 모든뱃지 반환하는 버그 수정

--- server/spring/src/main/java/com/tripick/mz/member/repository/MemberBadgeRepository.java
+++ server/spring/src/main/java/com/tripick/mz/member/repository/MemberBadgeRepository.java
@@ -6,10 +6,11 @@ import com.tripick.mz.member.entity.Member;
 import com.tripick.mz.member.entity.MemberBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
-    @Query("SELECT new com.tripick.mz.auth.dto.response.MemberBadgeResDto(mb.badge, mb.achieved, mb.achievedDate, mb.visitCount) FROM MemberBadge mb")
-    List<MemberBadgeResDto> findByMemberWithoutMemberInfo(Member memebr);
+    @Query("SELECT new com.tripick.mz.auth.dto.response.MemberBadgeResDto(mb.badge, mb.achieved, mb.achievedDate, mb.visitCount) FROM MemberBadge mb WHERE mb.member.memberId = :#{#member.memberId}")
+    List<MemberBadgeResDto> findByMemberWithoutMemberInfo(@Param("member") Member memebr);
 }
