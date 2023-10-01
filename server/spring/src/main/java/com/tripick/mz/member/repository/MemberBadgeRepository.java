b1fcb319ac813b07e6751c32c9931d9b936cc681
허성백 <heosb123@hanmail.net>
2023-10-01 19:22:09 +0900

    feat: 초기 계정 생성 시 뱃지 자동 획득

---
+++ server/spring/src/main/java/com/tripick/mz/member/repository/MemberBadgeRepository.java
@@ -0,0 +1,15 @@
+package com.tripick.mz.member.repository;
+
+import com.tripick.mz.auth.dto.response.MemberBadgeResDto;
+import com.tripick.mz.member.entity.Badge;
+import com.tripick.mz.member.entity.Member;
+import com.tripick.mz.member.entity.MemberBadge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
+    @Query("SELECT new com.tripick.mz.auth.dto.response.MemberBadgeResDto(mb.badge, mb.achieved, mb.achievedDate, mb.visitCount) FROM MemberBadge mb")
+    List<MemberBadgeResDto> findByMemberWithoutMemberInfo(Member memebr);
+}
