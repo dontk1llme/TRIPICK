27220feff28ac33a60f49dc8572a1adc8b347895
허성백 <heosb123@hanmail.net>
2023-10-01 22:44:09 +0900

    [FEATURE] 로그아웃 기능, 회원 탈퇴 기능 구현

--- server/spring/src/main/java/com/tripick/mz/auth/controller/AuthController.java
+++ server/spring/src/main/java/com/tripick/mz/auth/controller/AuthController.java
@@ -1,16 +1,18 @@
 package com.tripick.mz.auth.controller;
 
+import com.tripick.mz.auth.dto.request.LogoutReqDto;
+import com.tripick.mz.auth.dto.request.MemberDeleteReqDto;
 import com.tripick.mz.auth.dto.response.LoginResponseDto;
+import com.tripick.mz.auth.service.AuthService;
 import com.tripick.mz.auth.service.GoogleAuthService;
 import com.tripick.mz.auth.service.KakaoAuthService;
 import com.tripick.mz.auth.util.HeaderUtil;
+import com.tripick.mz.common.response.ResponseResult;
 import com.tripick.mz.common.response.SingleResponseResult;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class AuthController {
 
     private final KakaoAuthService kakaoAuthService;
     private final GoogleAuthService googleAuthService;
+    private final AuthService authService;
     private final HeaderUtil headerUtil;
 
     @GetMapping("/login/kakao")
@@ -35,6 +38,18 @@ public class AuthController {
         LoginResponseDto loginResponseDto = googleAuthService.googleLogin(code);
         return ResponseEntity.ok().headers(headerUtil.setTokenHeaders(loginResponseDto.getTokenDto()))
                 .body(new SingleResponseResult<>(loginResponseDto.getMemberResDto()));
+    }
+
+    @PatchMapping("/logout")
+    public ResponseResult logout(@RequestBody LogoutReqDto logoutReqDto){
+        authService.logout(logoutReqDto);
+        return ResponseResult.successResponse;
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseResult deleteMember(@RequestBody MemberDeleteReqDto memberDeleteReqDto){
+        authService.deleteMember(memberDeleteReqDto);
+        return ResponseResult.successResponse;
     }
 
 }
