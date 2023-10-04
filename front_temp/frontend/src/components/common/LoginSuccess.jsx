38f6dce22cad314bca97c43f35c030798a92dbf7
dontk1llme <letm2go@gmail.com>
2023-10-04 12:55:49 +0900

    feat: login state try1
    Former-commit-id: f8b73e1d66c94241a6f08bbe9ad576f92c2296f9

--- front_temp/frontend/src/components/common/LoginSuccess.jsx
+++ front_temp/frontend/src/components/common/LoginSuccess.jsx
@@ -1,9 +1,17 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useNavigate, useState } from 'react';
+import * as utils from 'utils';
 import * as api from 'api';
 import { useGoogleLogin } from '@react-oauth/google';
+import { create } from 'lodash';
 
 const LoginSuccess = () => {
+    const navigate = useNavigate();
+    const [ memberId, setMemberId,] = useState('');
+    const [ nickname, setNickname,] = useState('');
+    const [ email, setEmail,] = useState('');
+    const [ profileImage, setProfileImage,] = useState('');
+    const [ createdAt, setCreatedAt,] = useState('');
+
     useEffect(() => {
         const code = new URL(window.location.href).searchParams.get('code');
         if (code) {
@@ -11,6 +19,14 @@ const LoginSuccess = () => {
                 .createLoginRequest(code)
                 .then(response => {
                     console.log(response);
+                    // response를 state에 저장
+                    setMemberId(response.memberId);
+                    setNickname(response.nickname);
+                    setEmail(response.email);
+                    setProfileImage(response.profileImage);
+                    setCreatedAt(response.createdAt);
+                    
+                    navigate(utils.URL.HOME.LANDING);
                 })
                 .catch(error => console.error(error));
         }
