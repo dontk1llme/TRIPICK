d9e02dd9b5dd1b8152beeb863129e6cf36ed1502
dontk1llme <letm2go@gmail.com>
2023-10-04 17:03:26 +0900

    fix: accesstoken test 2
    Former-commit-id: 6c51f7fd5d7b626a5a9f0e66a611122b21cba03a

--- front_temp/frontend/src/api/api.js
+++ front_temp/frontend/src/api/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import * as utils from 'utils';
 import * as hooks from 'hooks';
 
+const {accessToken} = hooks.loginUserState();
 export const instance = axios.create({
     // baseURL: utils.API_BASE_URL,
     baseURL: 'https://tripick.site',
@@ -9,10 +10,11 @@ export const instance = axios.create({
     headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         Accept: 'application/json',
-        Authorization: '',  //response에 잇는 header 에서 뽑아서 여기에 넣기.
+        // Authorization: `${accessToken}`,  //response에 잇는 header 에서 뽑아서 여기에 넣기.
     },
 });
 
+instance.defaults.headers.common['Authorization'] = accessToken;
 
 // 요청을 보내기 전에 인증 토큰을 Authorization 헤더에 추가
 // instance.interceptors.request.use(
