08364f0679c43eb10bc1067734422b8716992a81
dontk1llme <letm2go@gmail.com>
2023-10-04 14:02:12 +0900

    fix: login test 14
    Former-commit-id: e52e0bdf1d065764013a3a89a69721d690324e10

--- front_temp/frontend/src/components/common/LoginSuccess.jsx
+++ front_temp/frontend/src/components/common/LoginSuccess.jsx
@@ -13,8 +13,12 @@ const LoginSuccess = () => {
     // const [ email, setEmail] = useState('');
     // const [ profileImage, setProfileImage] = useState('');
     // const [ createdAt, setCreatedAt] = useState('');
-    const [ memberId, setMemberId ,  nickname, setNickname, email, setEmail, 
-        profileImage, setProfileImage, createdAt, setCreatedAt] = hooks.loginUserState();
+
+    // const [ memberId, setMemberId, nickname, setNickname, email, setEmail, 
+    //     profileImage, setProfileImage, createdAt, setCreatedAt ] = hooks.loginUserState();
+
+    const [setLoginUser] = hooks.loginUserState();
+
 
     useEffect(() => {
         const code = new URL(window.location.href).searchParams.get('code');
@@ -24,20 +28,20 @@ const LoginSuccess = () => {
                 .then(response => {
                     console.log(response);
                     //초기값 출력
-                    console.log(memberId, nickname, email, profileImage, createdAt);
+                    // console.log(memberId, nickname, email, profileImage, createdAt);
                     console.log(response.data.data.memberId);
                     console.log(response.data.data.nickname);
 
                     // response를 state에 저장
                     // set 안 되는 중. .. .
-                    setMemberId(response.data.data.memberId);
-                    setNickname(response.data.data.nickname);
-                    setEmail(response.data.data.email);
-                    setProfileImage(response.data.data.profileImage);
-                    setCreatedAt(response.data.data.createdAt);
-
+                    // setMemberId(response.data.data.memberId);
+                    // setNickname(response.data.data.nickname);
+                    // setEmail(response.data.data.email);
+                    // setProfileImage(response.data.data.profileImage);
+                    // setCreatedAt(response.data.data.createdAt);
+                    setLoginUser(response.data.data);
                     console.log('어앵');
-                    console.log(memberId, nickname, email, profileImage, createdAt);
+                    // console.log(memberId, nickname, email, profileImage, createdAt);
 
                     // navigate(utils.URL.HOME.LANDING);
                 })
